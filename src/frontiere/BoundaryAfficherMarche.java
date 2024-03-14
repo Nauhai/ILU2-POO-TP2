8bb8ba2887fb8f28cb52305ac047580b00cf028b
Noé Mastrorillo <noe.mastrorillo@gmail.com>
2024-03-14 19:50:13 +0100

    Cas afficher marche

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,19 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infosMarche = controlAfficherMarche.donnerInfosMarche();
+		if (infosMarche.length == 0) {
+			System.out.println("Le marché est vide, revenez plus tard.");
+		} else {
+			StringBuilder builder = new StringBuilder(nomAcheteur + ", vous trouverez au marché :");
+			int i = 0;
+			while (i < infosMarche.length) {
+				String vendeur = infosMarche[i++];
+				String quantite = infosMarche[i++];
+				String produit = infosMarche[i++];
+				builder.append("\n- " + vendeur + " qui vend " + quantite + " " + produit + "");
+			}
+			System.out.println(builder.toString());
+		}
 	}
 }
